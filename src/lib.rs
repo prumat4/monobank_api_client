5b3644b8cfe2b1c5daf6536d286f5d76f94b68e4
maksymdidukh <maxym.didukh@logivations.com>
2024-07-13 16:13:37 +0300

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use reqwest;
+use reqwest::{self, header::{HeaderMap, HeaderValue}};
 use serde::{Deserialize, Serialize};
+use serde::Deserializer;
 use serde_json;
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -8,9 +9,57 @@ pub struct ApiCurrencyJson {
     currencyCodeA: Option<i32>,
     currencyCodeB: Option<i32>,
     date: Option<i64>,
-    rateSell: Option<f32>,
-    rateBuy: Option<f32>,
-    rateCross: Option<f32>,
+    rateSell: Option<f64>,
+    rateBuy: Option<f64>,
+    rateCross: Option<f64>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[allow(non_snake_case)]
+pub struct Account {
+    id: String,
+    sendId: String,
+    #[serde(deserialize_with = "from_cents")]
+    balance: f32,
+    #[serde(deserialize_with = "from_cents")]
+    creditLimit: f32,
+    r#type: String,
+    currencyCode: i16,
+    cashbackType: Option<String>,
+    maskedPan: Vec<String>,
+    iban: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[allow(non_snake_case)]
+pub struct Jar {
+    id: String,
+    sendId: String,
+    title: String,
+    description: String,
+    currencyCode: i16,
+    #[serde(deserialize_with = "from_cents")]
+    balance: f32,
+    goal: Option<f64>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[allow(non_snake_case)]
+pub struct MonobankClientInfo {
+    clientId: String,
+    name: String,
+    webHookUrl: String,
+    permissions: String,
+    accounts: Vec<Account>,
+    jars: Vec<Jar>,
+}
+
+pub fn from_cents<'de, D>(deserializer: D) -> Result<f32, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let cents = i32::deserialize(deserializer)?;
+    Ok(cents as f32 / 100.0)
 }
 
 pub fn get_currencies() -> Result<String, Box<dyn std::error::Error>> {
@@ -19,5 +68,20 @@ pub fn get_currencies() -> Result<String, Box<dyn std::error::Error>> {
     let api_response: Vec<ApiCurrencyJson> = res.json()?;
     let pretty_json = serde_json::to_string_pretty(&api_response)?;
 
+    Ok(pretty_json)
+}
+
+pub fn req(api_key: &String) -> Result<String, Box<dyn std::error::Error>> {
+    let url = "https://api.monobank.ua/personal/client-info";
+    
+    let mut headers = HeaderMap::new();
+    headers.insert("X-Token", HeaderValue::from_str(api_key)?);
+    
+    let client = reqwest::blocking::Client::new();
+    let res = client.get(url).headers(headers).send()?;
+    
+    let api_response: MonobankClientInfo = res.json()?;
+    let pretty_json = serde_json::to_string_pretty(&api_response)?;
+    
     Ok(pretty_json)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY must be set");
 
-    match monobank_api::get_currencies() {
+    // match monobank_api::get_currencies() {
+    //     Ok(pretty_json) => println!("Currencies: {}", pretty_json),
+    //     Err(e) => eprint!("Error in get_currencies: {}", e)
+    // }
+
+    match monobank_api::req(&api_key) {
         Ok(pretty_json) => println!("Currencies: {}", pretty_json),
-        Err(e) => eprint!("Error in get_currencies: {}", e)
+        Err(e) => eprint!("Error in req: {}", e)
     }
 
     Ok(())
